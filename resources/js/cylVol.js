a5e29ca25f53cda5d1259068d9f1474cfff7e760
crileyarch <chris.riley@cprime.com>
2018-03-03 16:35:01 -0500

    CYLVOL-1: Added logic for javascript calc.

--- resources/js/cylVol.js
+++ resources/js/cylVol.js
@@ -2,4 +2,10 @@
 
 function calc_vol() {
 
+	var radius = document.getElementById("radius").value;
+	var height = document.getElementById("height").value;
+	var out = document.getElementById("volume");
+	volume.value = Math.PI*radius*radius*height;
+
+
 }
